3a032f9f3fd0cba9120ec0475bb7d9f05cf0eb13
Alex Tristram <allex.tristram.gmail.com>
2021-04-06 14:28:14 +0100

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+var cors = require("cors");
 const { handle500s, handle400s, handleCustomError } = require("./errors");
 const app = express();
 const apiRouter = require("./routes/apiRouter");
+
+app.use(cors());
 
 app.use(express.json());
 
