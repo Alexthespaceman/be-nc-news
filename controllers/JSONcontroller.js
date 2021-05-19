7154248877e3603f8e577e85be4abcec32b54b1b
Alex Tristram <allex.tristram.gmail.com>
2021-05-19 17:32:02 +0100

    hosting testing

--- controllers/JSONcontroller.js
+++ controllers/JSONcontroller.js
@@ -1,8 +1,7 @@
 const { fetchJSON } = require("../models/endpointsModel");
 
-console.log("in here");
-
 exports.getJSON = (req, res, next) => {
+  console.log("in controller");
   fetchJSON()
     .then((endPoints) => {
       return res.status(200).send(endPoints);
--- models/endpointsModel.js
+++ models/endpointsModel.js
@@ -1,7 +1,8 @@
 const fs = require("fs").promises;
 
 exports.fetchJSON = () => {
-  return fs.readFile("endpoints.JSON", "utf8").then((endPoints) => {
+  console.log("in model");
+  return fs.readFile("endpoints.json", "utf8").then((endPoints) => {
     return JSON.parse(endPoints);
   });
 };
